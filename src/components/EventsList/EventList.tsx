80d180a46073c63070d68ceca0fed27c1c2f77b3
Brandon <7310114+munyb@users.noreply.github.com>
2022-02-08 23:24:51 -0700

    Styling change

--- src/components/EventsList/EventList.tsx
+++ src/components/EventsList/EventList.tsx
@@ -1,57 +1,50 @@
 import React, { useEffect } from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Button, Col, Row, Table } from 'react-bootstrap';
 
 import { useTypedSelector } from '../../hooks/useTypedSelector';
-import { useActions } from '../../hooks/useActions';
 import './eventList.scss';
 
 const EventList = () => {
     const { data, loading, error } = useTypedSelector((state) => state.events);
-    const { getEvents } = useActions();
-
-    useEffect(() => {
-        getEvents();
-    }, []);
-
-    // useEffect(() => {
-    //     console.log(data);
-    // }, [data]);
 
     return (
-        <div>
-            {data.length > 0 &&
-                data.map((event, eventIndex: number) => {
-                    return (
-                        <div
-                            className="border m-3 p-3"
-                            key={`event-${eventIndex}`}
-                        >
-                            <div className="mb-2 d-flex justify-content-between">
-                                <div>
+        <Table className="events-list rounded bg-light">
+            <tbody>
+                {data.length > 0 &&
+                    data.map((event, eventIndex: number) => {
+                        return (
+                            <tr
+                                className="text-white border-dark"
+                                key={`event-${eventIndex}`}
+                            >
+                                <td className="p-3">
                                     {data[eventIndex].artists.map(
                                         (artist, artistIndex: number) => {
                                             return (
                                                 <div
                                                     key={`${event.event_id}-artist-${artistIndex}`}
                                                 >
-                                                    {artist.name}{' '}
-                                                    {artist.is_headliner &&
-                                                        '(H)'}
+                                                    {artist.is_headliner ? (
+                                                        <h3>{artist.name}</h3>
+                                                    ) : (
+                                                        <small>
+                                                            {artist.name}
+                                                        </small>
+                                                    )}
                                                 </div>
                                             );
                                         }
                                     )}
-                                </div>
-                                <div>{event.date}</div>
-                            </div>
-
-                            <div className="d-flex justify-content-between">
-                                <div>{event.venue.name}</div>
-                            </div>
-                        </div>
-                    );
-                })}
-        </div>
+                                </td>
+                                <td className="p-3">
+                                    <div>{event.date}</div>
+                                    <div>{event.venue.name}</div>
+                                </td>
+                            </tr>
+                        );
+                    })}
+            </tbody>
+        </Table>
     );
 };
 
